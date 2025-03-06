92c3ba17eebc73c225e62bb0a09819f47678158d
jameel-sc <jameelabudalal30@gmail.com>
2025-03-06 11:54:31 +0200

    create project

---
+++ js/myjs.js
@@ -0,0 +1,16 @@
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+//     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//       return new bootstrap.Tooltip(tooltipTriggerEl)
+//     })
+  
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
+    })
+//   });
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+  
